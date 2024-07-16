3347726b357731f2fdeda3d5d716c812e1d23660
gabrielvitorsd <163777402+gabrielvitorsd@users.noreply.github.com>
2024-07-16 01:49:27 -0300

    Update menu.go

--- src/hello/Módulo 2 - Trabalhando com variáveis/Atividade 3 - Exibindo o menu/menu.go
+++ src/hello/Módulo 2 - Trabalhando com variáveis/Atividade 3 - Exibindo o menu/menu.go
@@ -12,13 +12,19 @@ func main() {
 	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Este programa está na versão:", versao)
 	fmt.Println()
-	
+
 	//Menu de opções
 	fmt.Println("1 - Monitoramento do site")
 	fmt.Println("2 - Exibir os logs")
-	fmt.Println("3 - ")
 	fmt.Println("0 - Sair do programa")
+	fmt.Println()
 
+	//Entrada de dados pelo usuário = input
+	fmt.Println("Digite a opção desejada:")
+	var comando int //O comando que o usuário digita sendo 1,2 e 0 é um inteiro.
+	fmt.Scanf("%d", &comando) //O especificador "%d" é usado para ler um valor inteiro decimal e o & é usado para obter o endereço de uma variável.
+	fmt.Println("O endereço da minha variável comando é", &comando)
+	fmt.Println("O comando escolhido foi:", comando)
 }
 
 
