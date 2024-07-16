2998e6160cdfb4ec49e53d0ab0dd8e9601468ec3
gabrielvitorsd <163777402+gabrielvitorsd@users.noreply.github.com>
2024-07-15 22:10:08 -0300

    Atividade 3 - Exibindo o menu
    Adição da pasta para aparecer o menu de opções.

---
+++ src/hello/Módulo 2 - Trabalhando com variáveis/Atividade 3 - Exibindo o menu/menu.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	
+)
+
+func main() {
+	//Dados inicias
+	nome := "Gabriel"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão:", versao)
+	fmt.Println()
+	
+	//Menu de opções
+	fmt.Println("1 - Monitoramento do site")
+	fmt.Println("2 - Exibir os logs")
+	fmt.Println("3 - ")
+	fmt.Println("0 - Sair do programa")
+
+}
+
+
+
