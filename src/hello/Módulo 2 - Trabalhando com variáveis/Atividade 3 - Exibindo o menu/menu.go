b9af87d74f7b8fa6093a309d18fe0c7ed7bbdf07
gabrielvitorsd <163777402+gabrielvitorsd@users.noreply.github.com>
2024-07-16 11:04:57 -0300

    Update menu.go
    Diferença entre Scanf e Scan.

--- src/hello/Módulo 2 - Trabalhando com variáveis/Atividade 3 - Exibindo o menu/menu.go
+++ src/hello/Módulo 2 - Trabalhando com variáveis/Atividade 3 - Exibindo o menu/menu.go
@@ -21,11 +21,24 @@ func main() {
 
 	//Entrada de dados pelo usuário = input
 	fmt.Println("Digite a opção desejada:")
+	
 	var comando int //O comando que o usuário digita sendo 1,2 e 0 é um inteiro.
 	fmt.Scanf("%d", &comando) //O especificador "%d" é usado para ler um valor inteiro decimal e o & é usado para obter o endereço de uma variável.
+	//Na função Scanf temos sempre dois parâmetros sendo que o primeiro é % o modificador e o segundo & é o endereço.
 	fmt.Println("O endereço da minha variável comando é", &comando)
 	fmt.Println("O comando escolhido foi:", comando)
+	fmt.Println()
+
+	//Simplificação da função Scanf
+	fmt.Println("Digite a segunda opção desejada:")
+	var comando1 int 
+	fmt.Scan(&comando1) //Se você declarou a variável comando como int o Scan entende isso, logo simplifica o uso dessa função. Ele infere o tipo da variável.
+	fmt.Println("O endereço da minha variável comando é", &comando1)
+	fmt.Println("O comando escolhido foi:", comando1)	
+	fmt.Println()
 }
+
+
 
 
 
